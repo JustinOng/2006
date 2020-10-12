5454bd4a8d20bc65b2a46fd0da6ecd57a6f07b94
JONG114 <JONG114@e5efd241-da70-2d45-b98d-21ed194d24f4>
2020-10-12 13:45:04 +0000

    Add UI to display nearby carparks
    git-svn-id: https://172.21.145.41/svn/CX2006/SEP2/CLONS5@768 e5efd241-da70-2d45-b98d-21ed194d24f4

---
+++ Application/app/static/js/main.js
@@ -0,0 +1,33 @@
+window.onload = () => {
+  const map = L.map("map-container").setView([1.37, 103.8], 12);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  map.on("click", (e) => {
+    console.log(e.latlng);
+  });
+
+  L.control
+    .layers(null, {
+      Carparks: carpark_layer,
+    })
+    .addTo(map);
+
+  navigator.geolocation.watchPosition(
+    (position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+      load_carparks(
+        map,
+        position.coords.latitude,
+        position.coords.longitude,
+        10
+      );
+    },
+    (err) => {
+      console.error("Failed to retrieve geolocation:", err);
+    }
+  );
+};
