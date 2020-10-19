3bce2b1429217fd11499801a9e6870d86409e2ba
Justin Ong <justin.ong.97@gmail.com>
2020-10-19 08:06:21 +0800

    Shift carpark parsing logic into CarparkManager

--- Application/app/managers/CarparkManager.py
+++ Application/app/managers/CarparkManager.py
@@ -1,8 +1,50 @@
-from managers import APIManager
+from datetime import datetime, timedelta
 from haversine import haversine
 
+from managers import APIManager
+from entities import Carpark
+
+carpark_info = {}
+
 def get_nearby_carparks(lat, lon, radius):
-    carparks = APIManager.get_carparks_availability()
+    global carpark_info
+
+    if len(carpark_info) == 0:
+        carpark_info = APIManager.get_carparks_info()
+
+    avail = APIManager.get_carparks_availability()
+    carparks = []
+
+    for record in avail:
+        last_updated = datetime.fromisoformat(record["update_datetime"])
+        if last_updated < (datetime.now() - timedelta(hours=1)):
+            # ignore record if too old
+            continue
+
+        # there are certain IDs that are not present in info, just drop them
+        if record["carpark_number"] not in carpark_info:
+            continue
+
+        car_lots_available = None
+
+        for lot_info in record["carpark_info"]:
+            if lot_info["lot_type"] == "C":
+                car_lots_available = int(lot_info["lots_available"])
+        
+        if car_lots_available is None:
+            # this carpark has no car lots, we're not interested at all
+            continue
+
+        _id = record["carpark_number"]
+        carparks.append(Carpark.Carpark(
+            _id = _id,
+            name = carpark_info[_id]["address"],
+            available_lots = car_lots_available,
+            lot_type = "C",
+            latitude = carpark_info[_id]["latitude"],
+            longitude = carpark_info[_id]["longitude"],
+            last_updated = last_updated.isoformat()
+        ))
 
     user_loc = (lat, lon)
 
