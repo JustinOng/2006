123ac77b231f5a2dc319b13621e20e52400b3c2a
Justin Ong <justin.ong.97@gmail.com>
2020-10-31 17:45:50 +0800

    Refactor to use leaflet's location wrapper - set location on map click

--- Application/app/static/js/main.js
+++ Application/app/static/js/main.js
@@ -1,14 +1,23 @@
+let map;
+const pos_marker = L.marker();
+
+function onPosUpdate(evt) {
+  console.log("Got position update:", evt);
+  load_carparks(evt.latlng.lat, evt.latlng.lng, 5);
+
+  pos_marker.setLatLng(evt.latlng);
+  map.addLayer(pos_marker);
+}
+
 window.onload = () => {
-  const map = L.map("map-container").setView([1.37, 103.8], 12);
+  map = L.map("map-container").setView([1.37, 103.8], 12);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons from <a href="https://fontawesome.com/license">Font Awesome</a>',
   }).addTo(map);
 
-  map.on("click", (e) => {
-    console.log(e.latlng);
-  });
+  map.on("click", onPosUpdate);
 
   L.control
     .layers(null, {
@@ -19,20 +28,15 @@ window.onload = () => {
     })
     .addTo(map);
 
-  navigator.geolocation.watchPosition(
-    (position) => {
-      console.log(position.coords.latitude, position.coords.longitude);
-      load_carparks(
-        map,
-        position.coords.latitude,
-        position.coords.longitude,
-        10
-      );
-    },
-    (err) => {
-      console.error("Failed to retrieve geolocation:", err);
-    }
-  );
+  map.locate({
+    watch: true,
+  });
+
+  map.on("locationfound", onPosUpdate);
+
+  map.on("locationerror", (err) => {
+    console.error("Failed to retrieve location:", err);
+  });
 
   load_trafficimages_markers();
   load_alerts();
