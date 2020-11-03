9797e511abca15197737b329b13f6faa037487a4
Justin Ong <justin.ong.97@gmail.com>
2020-11-03 17:56:31 +0800

    Add generation of fake alerts

--- Application/app/managers/AlertManager.py
+++ Application/app/managers/AlertManager.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from managers import APIManager
 from entities.Alert import Alert
 
+fake_alerts = []
+
 def get_alerts():
     _, incidents = APIManager.get_dm_traffic_incidents()
 
@@ -25,12 +27,23 @@ def get_alerts():
             longitude = incident["Longitude"],
             msg = msg
         ))
-
-    if len(alerts) == 0:
-        alerts.append(Alert(
-            latitude = 1.3063822123608,
-            longitude = 103.84880530186572,
-            msg = "Fake incident for demo purposes!"
-        ))
     
-    return alerts
+    out = alerts + fake_alerts
+    fake_alerts.clear()
+    return out
+
+def add_fake_alerts():
+    data = {
+        "latitude": 1.323138012043208, 
+        "longitude": 103.8904226090564, 
+        "msg": "Vehicle breakdown on PIE (towards Changi Airport) at Paya Lebar Rd Exit.", 
+        "type": "Vehicle breakdown"
+    }
+
+    fake_alerts.append(Alert(
+        reportedDatetime = datetime.now(),
+        alertType = data["type"],
+        latitude = data["latitude"],
+        longitude = data["longitude"],
+        msg = data["msg"]
+    ))
