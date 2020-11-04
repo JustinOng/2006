3aa7f7da1711e39f0aea3a1f4233a848db7b3e2b
JONG114 <JONG114@e5efd241-da70-2d45-b98d-21ed194d24f4>
2020-11-04 15:45:29 +0000

    Use different icon for roadworks
    git-svn-id: https://172.21.145.41/svn/CX2006/SEP2/CLONS5@1285 e5efd241-da70-2d45-b98d-21ed194d24f4

--- Application/app/static/js/alerts.js
+++ Application/app/static/js/alerts.js
@@ -10,11 +10,22 @@ function loadAlerts() {
         iconAnchor: [Math.floor(32 / 2), Math.floor(32 / 2)],
       });
 
+      const roadworkIcon = L.divIcon({
+        html: `<img src="img/icons/tools-solid-grey.png" style="width: 100%; height: 100%"/>`,
+        iconSize: [32, 32],
+      });
+
       syncDisplay("alerts", alertLayer, data["alerts"], (layer, alert) => {
         if (layer === null) {
           layer = L.marker([alert["latitude"], alert["longitude"]], {
             icon: alertIcon,
           });
+
+          if (alert["type"] === "Roadwork") {
+            layer.setIcon(roadworkIcon);
+          } else {
+            layer.setIcon(alertIcon);
+          }
 
           layer.bindPopup(alert["msg"]);
         }
