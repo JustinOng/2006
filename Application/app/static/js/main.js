f9c81cef3fc9210686ee9c32c169b89c4829758c
Justin Ong <justin.ong.97@gmail.com>
2020-11-04 14:57:30 +0800

    Allow activation of map click to set marker

--- Application/app/static/js/main.js
+++ Application/app/static/js/main.js
@@ -9,6 +9,13 @@ function onPosUpdate(evt) {
   map.addLayer(pos_marker);
 }
 
+function enableMapClick() {
+  map.on("click", (evt) => {
+    console.log("click at", evt.latlng.lat, evt.latlng.lng);
+    onPosUpdate(evt.latlng.lat, evt.latlng.lng);
+  });
+}
+
 window.onload = () => {
   map = L.map("map-container", { zoomControl: false }).setView(
     [1.37, 103.8],
@@ -31,8 +38,6 @@ window.onload = () => {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons from <a href="https://fontawesome.com/license">Font Awesome</a> | Alert Sound: "Notification Up 1" by FoolBoyMedia of Freesound.org',
   }).addTo(map);
-
-  map.on("click", onPosUpdate);
 
   controlLayers = {
     Carparks: {
