cf09e296e5a5e785e8d446eea198bc9e4773c746
JONG114 <JONG114@e5efd241-da70-2d45-b98d-21ed194d24f4>
2020-11-04 15:45:23 +0000

    Change ERP icon if gantry is active
    git-svn-id: https://172.21.145.41/svn/CX2006/SEP2/CLONS5@1273 e5efd241-da70-2d45-b98d-21ed194d24f4

--- Application/app/static/js/erps.js
+++ Application/app/static/js/erps.js
@@ -1,5 +1,42 @@
 const erp_layer = L.layerGroup([]);
 
+function in_record(record, datetime) {
+  // if record is weekday but datetime is not weekday, return false
+  if (
+    record["DayType"] === "Weekdays" &&
+    !(datetime.getDay() >= 1 && datetime.getDay() <= 5)
+  ) {
+    return false;
+  }
+
+  // if record is Saturday but datetime is not saturday, return false
+  if (record["Saturdays"] === "Saturdays" && !(datetime.getDay() === 6)) {
+    return false;
+  }
+
+  const timestr = `${datetime
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${datetime.getMinutes().toString().padStart(2, "0")}`;
+
+  if (timestr >= record["startTime"] && timestr < record["endTime"]) {
+    return true;
+  }
+
+  return false;
+}
+
+function erp_active(gantry, datetime) {
+  for (const record of gantry["records"]) {
+    if (record["chargeAmount"] === 0) continue;
+    if (in_record(record, datetime)) {
+      return record;
+    }
+  }
+
+  return false;
+}
+
 function load_erps() {
   fetch("/api/ERPs/get")
     .then((res) => res.json())
@@ -7,7 +44,14 @@ function load_erps() {
       const erpIcon = L.icon({
         iconUrl: "img/icons/ERP.png",
         iconSize: [36, 15],
-        iconAnchor: [Math.floor(36 / 2), 0],
+        iconAnchor: [Math.floor(36 / 2), Math.floor(15 / 2)],
+        popupAnchor: [0, 0],
+      });
+
+      const erpIconInactive = L.icon({
+        iconUrl: "img/icons/ERP_inactive.png",
+        iconSize: [36, 15],
+        iconAnchor: [Math.floor(36 / 2), Math.floor(15 / 2)],
         popupAnchor: [0, 0],
       });
 
@@ -17,6 +61,20 @@ function load_erps() {
         });
 
         marker.bindPopup(gantry["name"]);
+        if (
+          erp_active(
+            gantry,
+            new Date(
+              "Sat Oct 30 2020 08:35:47 GMT+0800 (Singapore Standard Time)"
+            )
+          ) === false
+        ) {
+          marker.setIcon(erpIconInactive);
+          marker.setOpacity(0.5);
+          marker.setZIndexOffset(-500);
+        } else {
+          marker.setZIndexOffset(1000);
+        }
 
         erp_layer.addLayer(marker);
       }
